06cdbf266c204f23f9384e949459ab68cda024a8
Luke Boyle <me@lukeboyle.com>
2026-09-02 23:00:04 +1000

    center iccons on collapsed nav

--- src/components/NavHeader/components/utils.ts
+++ src/components/NavHeader/components/utils.ts
@@ -22,7 +22,7 @@ export function getNavLinkClassname({
     : 'text-muted-foreground hover:bg-accent hover:text-foreground';
 
   if (collapsed) {
-    return clsx(base, colorClassName, 'p-2.5');
+    return clsx(base, colorClassName, 'justify-center p-2.5');
   }
 
   return clsx(base, 'gap-3 px-3 py-2.5', colorClassName);
